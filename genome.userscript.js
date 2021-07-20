ab3e6f3d89b7b0aa1fd55a0d175d5256ec86b3bc
Diego Magalhães <diegogm@amazon.com>
2021-07-20 13:23:15 -0400

    Update to use MediumEditor

--- genome.userscript.js
+++ genome.userscript.js
@@ -2,11 +2,13 @@
 // @name         Genome Textarea WYSIWYG Editor
 // @namespace    https://github.com/dgomesbr/genome-textarea-us
 // @version      1.0
-// @description  Make Genome Textarea WYSIWYG 
-// @author       Ken Ng
-// @match        https://web.whatsapp.com/
-// @require      https://cdn.jsdelivr.net/npm/suneditor@latest/dist/suneditor.min.js
-// @resource     SUNEDITOR_CSS https://cdn.jsdelivr.net/npm/suneditor@latest/dist/css/suneditor.min.css
+// @description  Make Genome Textarea WYSIWYG
+// @author       diegogm
+// @match        *
+// @include      *
+// @require      https://cdn.jsdelivr.net/npm/medium-editor@latest/dist/js/medium-editor.min.js
+// @resource     MEDIUM_EDITOR_CSS https://cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css
+// @resource     THEME_CSS https://cdn.jsdelivr.net/npm/medium-editor@5.23.3/dist/css/themes/beagle.min.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -14,117 +16,31 @@
 /* jshint esversion: 6 */
 (function () {
     ('use strict');
+    const MEDIUM_EDITOR_CSS = GM_getResourceText("MEDIUM_EDITOR_CSS");
+    const THEME_CSS = GM_getResourceText("THEME_CSS");
 
-    const SUNEDITOR_CSS = GM_getResourceText("SUNEDITOR_CSS");
-    GM_addStyle(SUNEDITOR_CSS);
+    GM_addStyle(MEDIUM_EDITOR_CSS);
+    GM_addStyle(THEME_CSS);
 
-    const GENOME_ELEM_TARGET = "textarea";
-
-    const editor = SUNEDITOR.create((document.getElementById(GENOME_ELEM_TARGET)),
-        {
-            "textTags": {
-                "bold": "b",
-                "underline": "u",
-                "italic": "i",
-                "strike": "s"
-            },
-            "mode": "balloon",
-            "rtl": false,
-            "katex": "window.katex",
-            "resizingBar": false,
-            "showPathLabel": false,
-            "font": [
-                "Arial",
-                "tahoma",
-                "Courier New,Courier"
-            ],
-            "fontSize": [
-                8,
-                10,
-                14,
-                18,
-                24,
-                36
-            ],
-            "formats": [
-                "p",
-                "blockquote",
-                "h1",
-                "h2",
-                "h3"
-            ],
-            "colorList": [
-                [
-                    "#ff0000",
-                    "#ff5e00",
-                    "#ffe400",
-                    "#abf200"
-                ],
-                [
-                    "#00d8ff",
-                    "#0055ff",
-                    "#6600ff",
-                    "#ff00dd"
-                ]
-            ],
-            "imageResizing": false,
-            "imageHeightShow": false,
-            "imageFileInput": false,
-            "imageUrlInput": false,
-            "videoResizing": false,
-            "videoHeightShow": false,
-            "videoFileInput": false,
-            "videoUrlInput": false,
-            "videoRatioShow": false,
-            "audioUrlInput": false,
-            "tabDisable": false,
-            "lineHeights": [
-                {
-                    "text": "Single",
-                    "value": 1
-                },
-                {
-                    "text": "Double",
-                    "value": 2
-                }
-            ],
-            "paragraphStyles": [
-                "spaced",
-                {
-                    "name": "Box",
-                    "class": "__se__customClass"
-                }
-            ],
-            "buttonList": [
-                [
-                    "undo",
-                    "redo",
-                    "font",
-                    "fontSize",
-                    "formatBlock",
-                    "paragraphStyle",
-                    "blockquote",
-                    "bold",
-                    "underline",
-                    "italic",
-                    "strike",
-                    "subscript",
-                    "superscript",
-                    "fontColor",
-                    "hiliteColor",
-                    "textStyle",
-                    "removeFormat",
-                    "outdent",
-                    "indent",
-                    "align",
-                    "horizontalRule",
-                    "list",
-                    "lineHeight",
-                    "table",
-                    "link"
-                ]
-            ],
-            "lang": SUNEDITOR_LANG.en,
-            "lang(In nodejs)": "en"
-        });
+    let c = document.querySelector("[contenteditable]");
+    var editor = new MediumEditor(c, {
+        toolbar: {
+            /* These are the default options for the toolbar,
+           if nothing is passed this is what is used */
+            allowMultiParagraphSelection: true,
+            buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
+            diffLeft: 0,
+            diffTop: -10,
+            firstButtonClass: 'medium-editor-button-first',
+            lastButtonClass: 'medium-editor-button-last',
+            relativeContainer: null,
+            standardizeSelectionStart: false,
+            static: false,
+            /* options which only apply when static is true */
+            align: 'center',
+            sticky: false,
+            updateOnEmptySelection: false
+        }
+    });
+    console.log(editor);
 })();
